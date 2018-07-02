d33a3b250ed215f6d01dce233fccb9b69d6d01f5
ozzeh <ozzeh@pleaseignore.com>
2018-07-02 10:27:32 -0500

    checkpoint

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m grpc_tools.protoc -I proto --python_out=./ --grpc_python_out=./ proto/discordbot/main.proto
+python -m grpc_tools.protoc -I proto --python_out=./ --grpc_python_out=./ proto/grpc_discord/*.proto
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='grpc_discord',
+    version='0.1',
+    packages=['grpc_discord'],
+    install_requires = [
+        'grpcio',
+        'googleapis-common-protos'
+    ],
+    zip_safe = False
+)
