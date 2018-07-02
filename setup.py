9e7acba02bc038e4a32693eda28e849ed2f1693f
ozzeh <ozzeh@pleaseignore.com>
2018-07-02 13:27:20 -0500

    add grpcio back

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='grpc_discord',
     version='0.3',
     packages=['grpc_discord'],
     install_requires = [
+        'grpcio',
         'grpclib',
         'googleapis-common-protos'
     ],
