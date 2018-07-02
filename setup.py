84d6759832660ddd84e5fe0cbf9ff7e6f75604ef
ozzeh <ozzeh@pleaseignore.com>
2018-07-02 13:58:26 -0500

    bump to 0.5

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='grpc_discord',
-    version='0.4',
+    version='0.5',
     packages=['grpc_discord'],
     install_requires = [
+        'grpcio',
         'grpclib',
         'googleapis-common-protos'
     ],
